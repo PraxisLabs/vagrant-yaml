34715f0d88a75f99ed615712f45d2878d0216091
Christopher Gervais <chris@ergonlogic.com>
2013-02-06 12:34:39 -0500

    Use our own templates.

--- lib/vagrant-yaml.rb
+++ lib/vagrant-yaml.rb
@@ -6,5 +6,12 @@ require 'vagrant-yaml/commands/yaml_update'
 
 Vagrant.commands.register(:yaml) { VagrantYaml::Command::Yaml }
 
+module VagrantYaml
+  # The source root is the path to the root directory of the this gem.
+  def self.source_root
+    @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
+  end
+end
+
 # Add our custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
--- lib/vagrant-yaml/commands/yaml_init.rb
+++ lib/vagrant-yaml/commands/yaml_init.rb
@@ -17,18 +17,36 @@ module VagrantYaml
         argv = parse_options(opts)
         return if !argv
 
+        # Create our Vagrantfile
         save_path = @env.cwd.join("Vagrantfile")
         raise Errors::VagrantfileExistsError if save_path.exist?
 
-        template_path = ::Vagrant.source_root.join("templates/commands/init/Vagrantfile")
+        template_path = ::VagrantYaml.source_root.join("templates/Vagrantfile")
         contents = Vagrant::Util::TemplateRenderer.render(template_path,
                                                           :box_name => argv[0] || "base",
                                                           :box_url => argv[1])
-
-        # Write out the contents
         save_path.open("w+") do |f|
           f.write(contents)
         end
+
+        # Create our directories
+        Dir.mkdir('vms-available')
+        Dir.mkdir('vms-enabled')
+
+        # Create default.yml
+        save_path = @env.cwd.join("vms-available/default.yml")
+        raise Errors::VagrantfileExistsError if save_path.exist?
+
+        template_path = ::VagrantYaml.source_root.join("templates/default.yml")
+        contents = Vagrant::Util::TemplateRenderer.render(template_path,
+                                                          :box_name => argv[0] || "base",
+                                                          :box_url => argv[1])
+        save_path.open("w+") do |f|
+          f.write(contents)
+        end
+
+        # Create symlink
+        File.symlink("../vms-available/default.yml", "vms-enabled/default.yml")
 
         @env.ui.info(I18n.t("vagrant.plugins.yaml.commands.init.success"),
                      :prefix => false)
