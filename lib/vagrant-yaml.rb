ad00d25d33b93bdbf5ed6e1e1227edc82cde7808
Christopher Gervais <chris@ergonlogic.com>
2013-02-06 11:37:41 -0500

    Add module structure.

--- lib/vagrant-yaml.rb
+++ lib/vagrant-yaml.rb
@@ -2,7 +2,7 @@ require 'vagrant'
 #require 'vagrant-yaml/errors'
 require 'vagrant-yaml/command'
 
-Vagrant.commands.register(:yaml) { InitYaml }
+Vagrant.commands.register(:yaml) { VagrantYaml::Command::YamlInit }
 
 # Add our custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
--- lib/vagrant-yaml/command.rb
+++ lib/vagrant-yaml/command.rb
@@ -2,35 +2,41 @@ require 'optparse'
 require 'vagrant/util/template_renderer'
 require 'vagrant/command/base'
 
-class InitYaml < Vagrant::Command::Base
-  def execute
-    options = {}
+module VagrantYaml
+  class Command < Vagrant::Command::Base
 
-    opts = OptionParser.new do |opts|
-      opts.banner = "Usage: vagrant init-yaml [box-name] [box-url]"
-    end
+    class YamlInit < VagrantYaml::Command
+      def execute
+        options = {}
 
-    # Parse the options
-    argv = parse_options(opts)
-    return if !argv
+        opts = OptionParser.new do |opts|
+          opts.banner = "Usage: vagrant init-yaml [box-name] [box-url]"
+        end
 
-    save_path = @env.cwd.join("Vagrantfile")
+        # Parse the options
+        argv = parse_options(opts)
+        return if !argv
+
+        save_path = @env.cwd.join("Vagrantfile")
 #    raise Errors::VagrantfileExistsError if save_path.exist?
 
-    template_path = ::Vagrant.source_root.join("templates/commands/init/Vagrantfile")
-    contents = Vagrant::Util::TemplateRenderer.render(template_path,
-                                                      :box_name => argv[0] || "base",
-                                                      :box_url => argv[1])
+        template_path = ::Vagrant.source_root.join("templates/commands/init/Vagrantfile")
+        contents = Vagrant::Util::TemplateRenderer.render(template_path,
+                                                          :box_name => argv[0] || "base",
+                                                          :box_url => argv[1])
 
-    # Write out the contents
-    save_path.open("w+") do |f|
-      f.write(contents)
+        # Write out the contents
+        save_path.open("w+") do |f|
+          f.write(contents)
+        end
+
+        @env.ui.info(I18n.t("vagrant.plugins.yaml.commands.init.success"),
+                     :prefix => false)
+
+        # Success, exit status 0
+        0
+       end
     end
-
-    @env.ui.info(I18n.t("vagrant.plugins.yaml.commands.init.success"),
-                 :prefix => false)
-
-    # Success, exit status 0
-    0
-   end
+  end
 end
+
