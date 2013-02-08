1e193e81e8ae55607701f160171bd89b9c84a4b7
Christopher Gervais <chris@ergonlogic.com>
2013-02-08 16:26:12 -0500

    Fix update command.

--- lib/vagrant-yaml/commands/yaml_update.rb
+++ lib/vagrant-yaml/commands/yaml_update.rb
@@ -9,33 +9,65 @@ module VagrantYaml
       def execute
         options = {}
 
+        # Boolean whether we should actually go through with the update
+        # or not. This is true only if the "--force" flag is set or if the
+        # user confirms it.
+        do_update = false
+
         opts = OptionParser.new do |opts|
-          opts.banner = "Usage: vagrant yaml update [box-name] [box-url]"
+          opts.banner = "Usage: vagrant yaml update"
+
+          opts.on("-f", "--force", "Update without confirmation.") do |f|
+            options[:force] = f
+          end
         end
 
         # Parse the options
         argv = parse_options(opts)
         return if !argv
 
-        save_path = @env.cwd.join("Vagrantfile")
-#    raise Errors::VagrantfileExistsError if save_path.exist?
+        if options[:force]
+          do_update = true
+        else
+          choice = nil
+          begin
+            choice = @env.ui.ask(I18n.t("vagrant.plugins.yaml.commands.update.confirmation"))
 
-        template_path = ::Vagrant.source_root.join("templates/commands/init/Vagrantfile")
-        contents = Vagrant::Util::TemplateRenderer.render(template_path,
-                                                          :box_name => argv[0] || "base",
-                                                          :box_url => argv[1])
+          rescue Errors::UIExpectsTTY
+            # We raise a more specific error but one which basically
+            # means the same thing.
+            raise Errors::UpdateRequiresForce
+          end
+          do_update = choice.upcase == "Y"
+        end
+
+        if do_update
+          @logger.info("Updating project with latest Vagrantfile from vagrant-yaml.")
+          update
+        else
+          @logger.info("Not updating project since confirmation was declined.")
+          @env.ui.success(I18n.t("vagrant.plugins.yaml.commands.update.will_not_update"),
+                            :prefix => false)
+        end
+      end
+
+      def update
+        save_path = @env.cwd.join("Vagrantfile")
+
+        template_path = ::VagrantYaml.source_root.join("templates/Vagrantfile")
+        contents = Vagrant::Util::TemplateRenderer.render(template_path)
 
         # Write out the contents
         save_path.open("w+") do |f|
           f.write(contents)
         end
 
-        @env.ui.info(I18n.t("vagrant.plugins.yaml.commands.init.success"),
+        @env.ui.info(I18n.t("vagrant.plugins.yaml.commands.update.success"),
                      :prefix => false)
 
         # Success, exit status 0
         0
-       end
+      end
     end
   end
 end
